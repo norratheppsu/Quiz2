5d7b9762dfadaff5ca695521a3dd70f05386070f
oat <oat@LAPTOP-TKR4T2S7>
2020-10-15 12:00:00 +0700

    Quiz2

---
+++ src/Quiz02/PSU.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Quiz02;
+
+/**
+ *
+ * @author oat
+ */
+public class PSU {
+   public enum PSUCampus{
+     PHUKET, SONGKLA, PATTANI, TRANG, SURAT
+    }
+   public void PSUFaculty (){
+    
+}
+    
+}
